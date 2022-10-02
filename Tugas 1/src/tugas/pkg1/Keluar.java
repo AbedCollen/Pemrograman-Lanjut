cf8d2e85c8f992bd9b7868f469a9d99cbb1ad124
AbedCollen <114875170+AbedCollen@users.noreply.github.com>
2022-10-03 00:24:45 +0700

    Add files via upload

---
+++ Tugas 1/src/tugas/pkg1/Keluar.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tugas.pkg1;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Keluar implements Atm{
+
+    @Override
+    public void fungsi() {
+        System.exit(0);
+    }   
+}
